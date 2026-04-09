048ae9b66ed99be59738bca8096a417646509392
liam whitehouse <liam.whitehouse@bigant.com>
2026-04-09 10:55:24 +1000

    Added Ranger Skeleton to Spawner and fixed up its controller

--- Zombie_Shooter/Source/Zombie_Shooter/Private/Spawner/AISpawner.cpp
+++ Zombie_Shooter/Source/Zombie_Shooter/Private/Spawner/AISpawner.cpp
@@ -3,3 +3,14 @@
 
 #include "Spawner/AISpawner.h"
 
+TSubclassOf<AZombieCharacter> AAISpawner::SpawnRandomZombie()
+{
+	if (Zombies.IsEmpty() == true)
+	{
+		return nullptr;
+	}
+
+	int32 randomIndex = FMath::RandRange(0, Zombies.Num() - 1);
+
+	return Zombies[randomIndex];
+}
--- Zombie_Shooter/Source/Zombie_Shooter/Public/Spawner/AISpawner.h
+++ Zombie_Shooter/Source/Zombie_Shooter/Public/Spawner/AISpawner.h
@@ -6,12 +6,22 @@
 #include "Spawner/Spawner.h"
 #include "AISpawner.generated.h"
 
+class AZombieCharacter;
+
 /**
- * 
+ *
  */
 UCLASS()
 class ZOMBIE_SHOOTER_API AAISpawner : public ASpawner
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	UFUNCTION(BlueprintPure)
+	TSubclassOf<AZombieCharacter> SpawnRandomZombie();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Enemies to Spawn")
+	TArray<TSubclassOf<AZombieCharacter>> Zombies;
+
 };
