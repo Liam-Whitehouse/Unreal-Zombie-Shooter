8cada5afaa4f81928d3006cf709823bcd79cc09d
lman888 <liamw98@hotmail.com>
2026-04-18 15:06:29 +1000

    Added Server Target

--- Zombie_Shooter/Intermediate/Build/BuildRulesProjects/Zombie_ShooterModuleRules/obj/Development/Zombie_ShooterModuleRules.AssemblyInfo.cs
+++ Zombie_Shooter/Intermediate/Build/BuildRulesProjects/Zombie_ShooterModuleRules/obj/Development/Zombie_ShooterModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zombie_ShooterModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d25e53dc710c7086029609caec03d53b1f5933f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d3cc74b74e45596c4cb5fb3575762e599a5b1bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zombie_ShooterModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zombie_ShooterModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
